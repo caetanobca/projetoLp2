474e4c9eefe6aac1e55d644aaef01df0462ec93a
medeirosavio <savio.medeiros@ccc.ufcg.edu.br>
2019-10-30 01:58:55 -0200

    Falta passar no easy

--- src/psquiza/Facade.java
+++ src/psquiza/Facade.java
@@ -2,9 +2,13 @@ package psquiza;
 
 public class Facade {
     private ControllerPesquisa controllerPesquisa;
+    private ControllerProblema controllerProblema;
+    private ControllerObjetivo controllerObjetivo;
 
     public Facade(){
         this.controllerPesquisa = new ControllerPesquisa();
+        this.controllerProblema = new ControllerProblema();
+        this.controllerObjetivo = new ControllerObjetivo();
     }
 
     public String cadastraPesquisa(String descricao, String campoDeInteresse){
@@ -30,4 +34,29 @@ public class Facade {
     public boolean pesquisaEhAtiva(String codigo){
         return this.controllerPesquisa.pesquisaEhAtiva(codigo);
     }
+
+    public String cadastraProblema(String descricao, int viabilidade) {
+        return this.controllerProblema.cadastraProblema(descricao,viabilidade);
+    }
+
+    public void apagarProblema(String codigo) {
+        this.controllerProblema.apagarProblema(codigo);
+    }
+
+    public String exibeProblema(String codigo) {
+        return this.controllerProblema.exibeProblema(codigo);
+    }
+
+    public String cadastraObjetivo(String tipo,String descricao,int aderencia,int viabilidade) {
+        return this.controllerObjetivo.cadastraObjetivo(tipo, descricao, aderencia, viabilidade);
+    }
+
+    public void apagarObjetivo(String codigo) {
+        this.controllerObjetivo.apagarObjetivo(codigo);
+    }
+
+    public String exibeObjetivo(String codigo) {
+        return this.controllerObjetivo.exibeObjetivo(codigo);
+    }
 }
+
