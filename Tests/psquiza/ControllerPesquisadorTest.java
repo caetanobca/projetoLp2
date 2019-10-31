2a279c17dd754c49a9b2b3c166879718e81bbe4a
Caetano <caetano.albuquerque@ccc.ufcg.edu.br>
2019-10-30 22:41:32 -0300

    testes

---
+++ Tests/psquiza/ControllerPesquisadorTest.java
@@ -0,0 +1,34 @@
+package psquiza;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerPesquisadorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cadastraPesquisador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void alteraPesquisador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ativaPesquisador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void desativaPesquisador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void exibePesquisador() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pesquisadorEhAtivo() {
+    }
+}
